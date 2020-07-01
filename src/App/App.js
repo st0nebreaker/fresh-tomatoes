1741e0858b5e6b2cc079aa472abecc1851d8f666
Andy Tom <andrew.tom@colorado.edu>
2020-06-30 18:41:33 -0600

    Error handling for fetch and styles

--- src/App/App.js
+++ src/App/App.js
@@ -7,35 +7,41 @@ class App extends Component {
 		super();
 		this.state = {
 			movies: [],
-			error: ''
+			error: null
 		}
 	}
 
 	componentDidMount() {
-		fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+		fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movie')
 			.then(res => res.json())
 			.then(
 				(result) => {
 					this.setState({
 						movies: result.movies
 					});
+				},
+				(error) => {
+					this.setState({
+						error
+					})
 				}
 			)
-			.catch(error => this.setState({error: error}))
 	}
 
 	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<h2>Decaying Ketchup</h2>
-					{/*TODO: add logo*/}
-					<button>Login</button>
-				</header>
-				{this.state.error && <h2>{this.state.error}</h2>}
-				<MovieContainer movies={this.state.movies} />
-			</div>
-		);
+		if (this.state.error) {
+			return <div className="error">Error: {this.state.error.message}</div>
+		} else {
+			return (
+				<div className="App">
+					<header className="App-header">
+						<h2>Decaying Ketchup</h2>
+						<button>Login</button>
+					</header>
+					<MovieContainer movies={this.state.movies} />
+				</div>
+			)
+		}
 	}
 }
 
