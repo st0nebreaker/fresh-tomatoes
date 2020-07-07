3415cf594ea2731dd450d8e2da72ee3349132ccb
Andy Tom <andrew.tom@colorado.edu>
2020-07-07 16:42:16 -0600

    adds ability to post ratings to API

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,79 +1,141 @@
-import React from 'react';
-import './MovieCard.scss';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "./MovieCard.scss";
+import { Link } from "react-router-dom";
+import { postRating } from "../apiCalls/apiCalls"
 
-const MovieCard = ({ userID, title, averageRating, poster, id, userRatings }) => {
-	let foundRating = null;
-	// console.log(userRatings)
-	if (userRatings) {
-		if (userRatings.find(rating => rating.movie_id === id)) {
-			foundRating = userRatings.find(rating => rating.movie_id === id).rating
+class MovieCard extends Component {
+	constructor({userID, title, averageRating, poster, id, userRatings}) {
+		super()
+		this.state = {
+			foundRating: null,
+			rating: null,
+		}
+		this.userID = userID
+		this.title = title
+		this.averageRating = averageRating;
+		this.poster = poster;
+		this.id = id;
+		this.userRatings = userRatings;
+	}
+
+	componentDidMount = () => {
+		if (this.userRatings) {
+		  if (this.userRatings.find((rating) => rating.movie_id === this.id)) {
+        	let foundRating = this.userRatings.find((rating) => rating.movie_id === this.id).rating;
+        	this.setState({ foundRating: foundRating });
+      		}
 		}
 	}
+
+	handleClick = (event) => {
+		event.preventDefault();
+		postRating(Number(this.state.rating), this.id, this.userID)
+	}
+
+	handleChange = (event) => {
+		this.setState({[event.target.name]: event.target.value})
+	}
+
+	render = () => {
 	
-	return (
-		<section className='movie-card-container' id={id}>
-			<section className='rating-form-container hide'>
-				<div><form className='rating-form'>
-					<div className='inputs'>
-						<label for="1">
-						<input type="radio" id="1" name="rating-action" value="1" />1</label>
-						<label for="2">
-						<input type="radio" id="2" name="rating-action" value="2"/>2</label>
-						<label for="3">
-						<input type="radio" id="3" name="rating-action" value="3"/>3</label>
-						<label for="4">
-						<input type="radio" id="4" name="rating-action" value="4"/>4</label>
-						<label for="5">
-						<input type="radio" id="5" name="rating-action" value="5"/>5</label>
-						<label for="6">
-						<input type="radio" id="6" name="rating-action" value="6"/>6</label>
-						<label for="7">
-						<input type="radio" id="7" name="rating-action" value="7"/>7</label>
-						<label for="8">
-						<input type="radio" id="8" name="rating-action" value="8"/>8</label>
-						<label for="9">
-						<input type="radio" id="9" name="rating-action" value="9"/>9</label>
-						<label for="10">
-						<input type="radio" id="10" name="rating-action" value="10"/>10</label>
-					</div>
-					<button
-            className="submit-rating-btn"
-            aria-label="submit-button"
-          >
-            Submit
-          </button>
-				</form></div>
-			</section>
-			<section className='movie-card' id={id}>
-				<section className='title-section'>
-					<h3>{title}</h3>
-				</section>
-				<section className="rating-section">
-					<p>
-						{foundRating ? 
-							<b className='user-rating-msg'>You rated {foundRating} </b> : 
-							`Average Rating ${Math.floor(averageRating)}`}/10
-					</p>
-					{averageRating >= 5 && <img className="rating-img" src= 'https://cdn.iconscout.com/icon/premium/png-256-thumb/tomato-1640383-1391081.png' alt= "Tomato" />}
-					{averageRating < 5 && <img className="rating-img" src= 'https://i.pinimg.com/originals/58/e0/a9/58e0a9b572353c77bb1a4b3f802f4cb8.png' alt= "Green Paint Splatter" />}
-				</section>
-				<Link to={`/movie_details/${id}`} >
-					<img src={poster} alt='movie poster' className='movie-poster' />
-				</Link>
-				{foundRating && 
-					<section className="rating-button-section">
-						<button className="rating-button">Delete rating</button>
-					</section>
-				}
-				{!foundRating && userID && 
-					<section className="rating-button-section">
-						<button className="rating-button">Add rating</button>
-					</section>
-				}
-			</section>
-		</section>
-	)
+    return (
+      <section className="movie-card-container" id={this.id}>
+        <section className="rating-form-container hide">
+          <div>
+            <form className="rating-form">
+              <div className="inputs">
+                <label htmlFor="1">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="1" name="rating" value="1" />1
+                </label>
+                <label htmlFor="2">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="2" name="rating" value="2" />2
+                </label>
+                <label htmlFor="3">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="3" name="rating" value="3" />3
+                </label>
+                <label htmlFor="4">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="4" name="rating" value="4" />4
+                </label>
+                <label htmlFor="5">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="5" name="rating" value="5" />5
+                </label>
+                <label htmlFor="6">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="6" name="rating" value="6" />6
+                </label>
+                <label htmlFor="7">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="7" name="rating" value="7" />7
+                </label>
+                <label htmlFor="8">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="8" name="rating" value="8" />8
+                </label>
+                <label htmlFor="9">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="9" name="rating" value="9" />9
+                </label>
+                <label htmlFor="10">
+                  <input onChange={(event) => this.handleChange(event)} type="radio" id="10" name="rating" value="10" />
+                  10
+                </label>
+              </div>
+              <button
+                onClick={(event) => this.handleClick(event)}
+                className="submit-rating-btn"
+                aria-label="submit-button"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </section>
+        <section className="movie-card" id={this.id}>
+          <section className="title-section">
+            <h3>{this.title}</h3>
+          </section>
+          <section className="rating-section">
+            <p>
+              {this.foundRating ? (
+                <b className="user-rating-msg">You rated {this.foundRating} </b>
+              ) : (
+                `Average Rating ${Math.floor(this.averageRating)}`
+              )}
+              /10
+            </p>
+            {this.averageRating >= 5 && (
+              <img
+                className="rating-img"
+                src="https://cdn.iconscout.com/icon/premium/png-256-thumb/tomato-1640383-1391081.png"
+                alt="Tomato"
+              />
+            )}
+            {this.averageRating < 5 && (
+              <img
+                className="rating-img"
+                src="https://i.pinimg.com/originals/58/e0/a9/58e0a9b572353c77bb1a4b3f802f4cb8.png"
+                alt="Green Paint Splatter"
+              />
+            )}
+          </section>
+          <Link to={`/movie_details/${this.id}`}>
+            <img
+              src={this.poster}
+              alt="movie poster"
+              className="movie-poster"
+            />
+          </Link>
+          {this.foundRating && (
+            <section className="rating-button-section">
+              <button className="rating-button">Delete rating</button>
+            </section>
+          )}
+          {!this.foundRating && this.userID && (
+            <section className="rating-button-section">
+              <button className="rating-button">Add rating</button>
+            </section>
+          )}
+        </section>
+      </section>
+    );
+
 }
+};
 
 export default MovieCard;
--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -27,3 +27,20 @@ export const verifyLogin = async (givenEmail, givenPassword) => {
   const data = await response.json();
   return data;
 };
+
+export const postRating = async (rating, movieID, userID) => {
+  console.log(rating, movieID, userID)
+  const request = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      movie_id: movieID,
+      rating: rating,
+    }),
+  };
+
+  const response = await fetch(`${rootUrl}users/${userID}/ratings`, request);
+  const data = await response.json();
+  return data;
+};
+
