b0efe7e59998a71ce90831f90b53741b79c3ba09
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-07-08 16:09:18 -0600

    testing added

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -4,56 +4,64 @@ import App from './App';
 import { getAllMovies } from '../apiCalls/apiCalls';
 
 import { BrowserRouter } from 'react-router-dom';
-import { render, wait } from '@testing-library/react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 jest.mock('../apiCalls/apiCalls');
 
-getAllMovies.mockResolvedValue(() => {
-	return Promise.resolve({
-		movies: [
-		{
-				id: 475430,
-				poster_path: "https://image.tmdb.org/t/p/original//tI8ocADh22GtQFV28vGHaBZVb0U.jpg",
-				backdrop_path: "https://image.tmdb.org/t/p/original//o0F8xAt8YuEm5mEZviX5pEFC12y.jpg",
-				title: "Artemis Fowl",
-				average_rating: 6.333333333333333,
-				release_date: "2020-06-12"
-		}]})
-	})
+getAllMovies.mockResolvedValue(() => [
+	{
+			id: 475430,
+			poster_path: "https://image.tmdb.org/t/p/original//tI8ocADh22GtQFV28vGHaBZVb0U.jpg",
+			backdrop_path: "https://image.tmdb.org/t/p/original//o0F8xAt8YuEm5mEZviX5pEFC12y.jpg",
+			title: "Artemis Fowl",
+			average_rating: 6.333333333333333,
+			release_date: "2020-06-12"
+	}
+])
 
 describe(('App'), () => {
-  it.skip('renders title to the page', () => {
-    const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
-    const linkElement = getByText(/Decaying Ketchup/i);
-    expect(linkElement).toBeInTheDocument();
+	it('renders the landing page without breaking', () => {
+		const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
+		const linkElement = getByText(/Fresh Tomatoes/);
+		expect(linkElement).toBeInTheDocument();
+	})
+
+	// it("should render a movie", async () => {
+  //   const {getByText, findByText} = render(<BrowserRouter><App /></BrowserRouter>);
+
+  //   const movieRating = await findByText('Average Rating')
+  //   expect(movieRating).toBeInTheDocument();
+  // });
+
+  it('should render login button to page', () => {
+    const { getByRole } = render(<BrowserRouter><App /></BrowserRouter>);
+    const loginBtn = getByRole('button');
+    expect(loginBtn).toBeInTheDocument();
   })
 
-  it.skip('renders Login button to the page', () => {
-    const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
-    const linkElement = getByText(/Login/i);
-    expect(linkElement).toBeInTheDocument();
-  })
+  it('when clicked, the login button should route to login page', () => {
+    const { getByRole } = render(<BrowserRouter><App /></BrowserRouter>);
+		const loginBtn = getByRole('button');
+		fireEvent.click(loginBtn);
 
-  it.skip("Should render the heading", () => {
+    expect(getByRole('link', {href: '/login'})).toBeInTheDocument();
+	})
+	
+	// it('should fetch movies on page load', async () => {
+  //   const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
+
+  //   // const linkeElement = await waitFor(() => getByText('Fresh Tomatoes'));
+  //   // expect(linkeElement).toBeInTheDocument();
+
+  //   const movieOne = await waitFor(() => getByText('Artemis Fowl')); 
+  //   expect(movieOne).toBeInTheDocument();
+    
+  // });
+
+  it("Should render the heading", () => {
     const { getByRole } = render(<BrowserRouter><App /></BrowserRouter>);
     const heading = getByRole("heading");
     expect(heading).toBeInTheDocument();
   });
-
-  it.skip('Should populate movie cards on page load', () => {
-		const { getByText, getByRole, getByPlaceholderText } = render(<BrowserRouter><App /></BrowserRouter>);
-
-		const movieTitle = getByText('Artemis Fowl');
-
-		expect(movieTitle).toBeInTheDocument();
-	})
-
-	it('Should render movies from server when loaded', async () => {
-		const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
-
-		const mockedTitle = await wait(() => getByText('Artemis Fowl'));
-
-		expect(mockedTitle).toBeInTheDocument();
-	})
 })
 
