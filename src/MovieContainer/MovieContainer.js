df45d6f59a9248c3751bd0cbf1bd52a81797429c
Andy Tom <andrew.tom@colorado.edu>
2020-07-11 13:04:46 -0600

    Refactor displaying favorites

--- src/MovieContainer/MovieContainer.js
+++ src/MovieContainer/MovieContainer.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom'
 import './MovieContainer.css';
 
 const MovieContainer = ({ appState, getUsersRatings, getAllFavorites, favorites }) => {
-  let movieCards;
+  let movies, movieCards;
   const user = appState.usersFavorites.find(user => user.user_id === appState.userID)
+
   if(!favorites) {
-    movieCards = appState.movies.map((movie, i) => {
+    movies = appState.movies
+  } else {
+    movies = appState.movies.filter(movie => user.movie_ids.includes(movie.id))
+  }
+
+  movieCards = movies.map((movie, i) => {
       return (
         <MovieCard
           userID={appState.userID}
@@ -22,31 +28,9 @@ const MovieContainer = ({ appState, getUsersRatings, getAllFavorites, favorites 
           getAllFavorites={getAllFavorites}
         />
       );
-    });
-  } else {
-    movieCards = appState.movies.map((movie, i) => {
-      if(user.movie_ids.includes(movie.id)) {
-        return (
-          <MovieCard
-            userID={appState.userID}
-            title={movie.title}
-            averageRating={movie.average_rating}
-            poster={movie.poster_path}
-            id={movie.id}
-            userRatings={appState.userRatings}
-            key={i}
-            getUsersRatings={getUsersRatings}
-            usersFavorites={appState.usersFavorites}
-            getAllFavorites={getAllFavorites}
-          />
-          );
-      } else {
-        return null;
-      }
-    });
-  }
+  })
   
-  if(movieCards.every(card => card === null)) {
+  if(!movies.length) {
     movieCards = (
     <div className="no-favorites-container">
     <Link to={`/`}><button className='back-btn'>◀ BACK</button></Link>
